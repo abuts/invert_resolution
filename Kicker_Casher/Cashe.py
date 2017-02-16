0eaa3454fb726579e8305e1f7c63c3e3875e489b
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-02-16 12:35:31 +0000

    Kicker_Casher for isiscompute (initial commits)

---
+++ Kicker_Casher/Cashe.py
@@ -0,0 +1,12 @@
+#/usr/bin/python
+from multiprocessing import Pool
+
+def process_file(filename):
+    """ """
+    fh = fopen(filename,'wb')
+    if fh<0:
+        
+
+
+if __name__ == '__main__':
+    argi 
