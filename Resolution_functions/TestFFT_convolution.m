06cba8141dd58b6c117a9e92138b0847230313d8
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-12-27 12:00:25 +0000

    test convolution: correct phase shift for any integration interval

--- Resolution_functions/TestFFT_convolution.m
+++ Resolution_functions/TestFFT_convolution.m
@@ -1,7 +1,8 @@
 function TestFFT_convolution()
 dt = 0.01;
-t_min= -30;
-t_max = 30;
+t_min= -10;
+t_max = 5;
+dT = t_max-t_min;
 t=t_min:dt:t_max;
 s = signal(t);
 Nt = numel(t);
@@ -17,15 +18,15 @@ fs = fft(s);
 fp = fft(pls);
 
 find = fft_ind(numel(fs));
-phase = 1i*pi*find; % for symmetric fft interval only
+phase = 1i*pi*find*(1-(t_min+t_max)/dT); % 
 
 fc = fs.*fp.*exp(phase);
-conv = ifft(fc)/(t_max-t_min);
+conv = ifft(fc)/dT;
 plot(t,conv);
 hold off
 
 function pls = pulse(t)
-t0 = -5;
+t0 = 2;
 sig1 = 0.05;
 sig12 = sig1*sig1;
 pls = abs(t).*exp(-(t-t0).^2/(2*sig12))/sqrt(2*pi*sig1);
