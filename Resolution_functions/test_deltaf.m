3b5372cab30dd89d3ac1852697bf7cb8402a9046
Alex Buts <Alex.Buts@stfc.ac.uk>
2018-11-05 15:08:01 +0000

    testing Fourier filtering. Standard FFT examples

--- Resolution_functions/test_deltaf.m
+++ Resolution_functions/test_deltaf.m
@@ -3,9 +3,9 @@ function test_deltaf
 %
 
 % check fft(Delta(v-V0)) = Sum_n{exp(-i*Omega_n*V0)}
-Np=33;
-iV = 19;
-RangeMin = 1;
+Np=32;
+iV = 3;
+RangeMin = 2;
 RangeMax =  3;
 Range0 = 0.5*(RangeMax+RangeMin);
 Range = RangeMax-RangeMin;
@@ -23,39 +23,43 @@ DeltaF1(2,iV+4) = 1;
 %omg = fftshift(Omega_n);
 %DeltaF1 = exp(1i*omg(iV)*xx);
 
-spec = fft(DeltaF1);
-delR = ifft(spec);
+fft_spec = fft(DeltaF1);
+rev_fft_data = ifft(fft_spec);
 theor_sp = exp(-1i*(V0-Range0)*Omega_n);
 %theor_sp = zeros(size(DeltaF1)); %
 %theor_sp(iV) = Np*exp(1i*omg(iV)*Range0);
-theoR = ifft(theor_sp);
-theor_rev = (sum((exp(1i*(xx-V0).*Omega_n')),1))/Np;
-[omg1,sf1] = sft(xx,DeltaF1,ind);
-[myt,myf] = isft(omg1,sf1,xx);
+ref_theor_spec = ifft(theor_sp);
+rev_theor_spec = (sum((exp(1i*(xx-V0).*Omega_n')),1))/Np;
+[sft_omega,sft_spec] = sft(xx,DeltaF1,ind);
+[sft_t,rev_sft_spec] = isft(sft_omega,sft_spec,xx);
 %theor_rev = (sum((exp(1i*(xx).*Omega_n')),1))/(Np-1);
-assertElementsAlmostEqual(DeltaF1,myf)
-plot(xx,real(DeltaF1),':',xx,real(delR),'*',xx,real(theor_rev),'o',xx,real(theoR),myt,myf,'+');
-if any(any(abs(imag(DeltaF1'))>1.e-9)) || any(any(abs(imag(delR'))>1.e-9)) || any(abs(imag(theoR))>1.e-9)
-    hold on;
-    plot(xx,imag(DeltaF1),':',xx,imag(delR),'*',xx,imag(theoR));
-    hold off;
-end
+assertElementsAlmostEqual(DeltaF1,rev_sft_spec)
+plot(xx,real(DeltaF1),':', xx,real(rev_fft_data),'*',xx,real(rev_theor_spec),'o',xx,real(ref_theor_spec),sft_t,real(rev_sft_spec),'+');
+legend('original','reversed fft','reversed theor','rev theor using ifft','reversed sft');
+% if any(any(abs(imag(DeltaF1'))>1.e-9)) || any(any(abs(imag(rev_fft_data'))>1.e-9)) || any(abs(imag(ref_theor_spec))>1.e-9)
+%     hold on;
+%     plot(xx,imag(DeltaF1),':',xx,imag(rev_fft_data),'*',xx,imag(ref_theor_spec));
+%     hold off;
+% end
 
 
 
-expT = IX_dataset_1d(fftshift(Omega_n/(2*pi)),(180/pi)*fftshift(atan2(imag(spec(1,:)),real(spec(1,:)))));
+expT = IX_dataset_1d(fftshift(Omega_n/(2*pi)),(180/pi)*fftshift(atan2(imag(fft_spec(1,:)),real(fft_spec(1,:)))));
+%expT = IX_dataset_1d((Omega_n/(2*pi)),(180/pi)*(atan2(imag(spec(1,:)),real(spec(1,:)))));
 acolor('r');
 aline('-');
 dl(expT);
 
 theorR = IX_dataset_1d(fftshift(Omega_n/(2*pi)),(180/pi)*fftshift(atan2(imag(theor_sp),real(theor_sp))));
+%theorR = IX_dataset_1d((Omega_n/(2*pi)),(180/pi)*(atan2(imag(theor_sp),real(theor_sp))));
 acolor('b');
-aline('-');
+aline(':');
 pl(theorR);
 
-sft_s = IX_dataset_1d(fftshift(omg1/(2*pi)),(180/pi)*fftshift(atan2(imag(sf1(1,:)),real(sf1(1,:)))));
+sft_s = IX_dataset_1d(fftshift(sft_omega/(2*pi)),(180/pi)*fftshift(atan2(imag(sft_spec(1,:)),real(sft_spec(1,:)))));
+%sft_s = IX_dataset_1d((omg1/(2*pi)),(180/pi)*(atan2(imag(sf1(1,:)),real(sf1(1,:)))));
 acolor('g');
-aline(':');
+aline('-');
 pl(sft_s );
 
 
---
+++ Resolution_functions/test_fftf_1D.m
@@ -0,0 +1,38 @@
+function test_fftf_1D
+% testing filter, related to standard fft transformation.
+
+t = (1:10000)*0.01;
+
+fd = fdistr(t);
+plot(t,fd);
+distr = build_distribution(t,fd,100000);
+
+[bedg,bin] = build_bins(t);
+fc = histcounts(distr,bedg);
+fc = fc./bin;
+hold on;
+plot(t,fc);
+
+
+[X, f, y, y2] = fftf_1D(t, fc, [],20);
+
+[X, f, y, y2] = fftf_1D(t, fc, [],10);
+[X, f, y, y2] = fftf_1D(t, fc, [],50);
+
+
+
+
+
+
+
+
+function fd = fdistr(t)
+
+t_min = min(t);
+t_max = max(t);
+dT = t_max-t_min;
+t0 =[0.1,0.55,0.7]*dT;
+Sigma = [sqrt(0.0002),sqrt(0.0010),sqrt(0.0050)]*dT;
+Sigma2 =Sigma.*Sigma;
+A = [2,3,5];
+fd = sum(exp(-(t-t0').^2./(2*Sigma2')).*(A'./Sigma')/sqrt(2*pi),1);
